1eea58241fbee2ebb9db97e3413ea1e3541746ae
NitHub04 <98111271+NitHub04@users.noreply.github.com>
2023-01-18 18:35:42 +0000

    Update st.cardioama.py

--- st.cardioama.py
+++ st.cardioama.py
@@ -1,3 +1,18 @@
+
+# Import from standard library
+import logging
+import random
+import re
+
+# Import from 3rd party libraries
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Import modules
+import tweets as twe
+import oai
+
+
 # Import from standard library
 import logging
 import re
@@ -7,13 +22,13 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 # Import modules
-import openai
-
-#OpenAI key
-openai.api_key = "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"
+import opanai
 
 # Configure logger
 logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, force=True)
+
+#OpenAI key
+openai.api_key = "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"
 
 response = openai.Completion.create(
     engine="text-davinci-002",
