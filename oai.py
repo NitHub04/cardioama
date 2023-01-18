56ef814c1fb3bacd60acb7ce4760518478a81715
NitHub04 <98111271+NitHub04@users.noreply.github.com>
2023-01-18 18:41:52 +0000

    Update oai.py

--- oai.py
+++ oai.py
@@ -9,7 +9,7 @@ import openai
 import streamlit as st
 
 # Assign credentials from environment variable or streamlit secrets dict
-openai.api_key = os.getenv("OPENAI_API_KEY") or st.secrets["OPENAI_API_KEY"]
+openai.api_key = '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'
 
 # Suppress openai request/response logging
 # Handle by manually changing the respective APIRequestor methods in the openai package
