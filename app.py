28baafda15361326d44985842d900bb35a24c45b
NitHub04 <98111271+NitHub04@users.noreply.github.com>
2023-01-20 11:38:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ import streamlit as st
 openai.api_key = st.secrets["api_secret"]
 
 """
-# Welcome to My Hospital Chatbot!
+# Welcome to your Barts Hospital cardiology chatbot!
 Feel free to ask any questions below:
+
+Note:
+This app is for research purposes only and the answers given can be wrong.
+This is not medical advice.
 """
 
 chatbot_input = st.text_input('Ask a question?','What is an angiogram?')
@@ -19,3 +23,7 @@ response = openai.Completion.create(
 answer = response["choices"][0]["text"]
 st.write(answer)
 print(answer)
+
+"""
+Please email your feedback to bartshealth.bartsaf@nhs.net
+"""
