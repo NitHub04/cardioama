e2c7afbe37948a00ac4dd45e2581f9478151e464
NitHub04 <98111271+NitHub04@users.noreply.github.com>
2023-01-20 12:23:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import streamlit as st
 openai.api_key = st.secrets["api_secret"]
 
 """
-# Welcome to your Barts Hospital cardiology chatbot!
+st.title('Welcome to your Barts Hospital cardiology chatbot!')
 
 Note:
 This app is for research purposes only and the answers given can be wrong.
 This is not medical advice.
 """
 
-chatbot_input = st.text_input('Ask your question below','e.g. what is a pacemaker?...')
+chatbot_input = st.text_input(st.subheader('Ask your question below'),'e.g. what is a pacemaker?...')
 
 response = openai.Completion.create(
     engine="text-davinci-002",
     prompt=chatbot_input,
-    temperature=0.5,
+    temperature=0.1,
     max_tokens=4000
 )
 
