7bf71e69810778d5c502ad3b12447d83b35be687
NitHub04 <98111271+NitHub04@users.noreply.github.com>
2023-01-29 20:24:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import openai
 import streamlit as st
+from deta import Deta
+from datetime import datetime
+import pandas as pd
 
 openai.api_key = st.secrets["api_secret"]
+deta = Deta(st.secrets["deta_key"])
+db = deta.Base("cardioama_db")
 
 """
 # Welcome to St Barts Cardiology Chatbot!
@@ -14,19 +19,29 @@ This is not medical advice.
 
 chatbot_input = st.text_input('Ask a question?','What is an angiogram?')
 
-response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=chatbot_input,
-    temperature=0.1,
-    max_tokens=4000
-)
+if st.button("Submit"):
+    
+        dateTimeObj = datetime.now()
+        timestampStr = dateTimeObj.strftime("%d-%b-%Y_%H:%M:%S")
+        response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=chatbot_input,
+        temperature=0.1,
+        max_tokens=4000
+    )
 
-answer = 'BartsChatbot:'+response["choices"][0]["text"]
+    answer = 'BartsChatbot:'+response["choices"][0]["text"]
 # st.button(label="Submit", help='press to ask your question', on_click=answer)
 
-st.write(answer)
-print(answer)
+    st.write(answer)
+    print(answer)
+    db.put({"a_time_stamp": timestampStr,"b_input": chatbot_input,"c_answer": answer })
 
 """
 Please email your feedback to bartshealth.bartsaf@nhs.net
 """
+
+if st.button('Display'):
+    db_content = db.fetch().items
+    df = pd.DataFrame.from_dict(db_content)
+    st.dataframe(df)
